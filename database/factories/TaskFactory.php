3392de44b55a33f4590e7f7f36e5176dccd8c793
solonight <thenewpage500@gmail.com>
2025-09-10 11:37:53 +0100

    initial commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement(['to_do','in_progress','done' ]),
+            'assigned_user_id' => User::factory(),
+            'created_by' => User::factory(),
+           
+        ];
+    }
+}
